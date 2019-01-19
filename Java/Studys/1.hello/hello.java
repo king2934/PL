03b71eff8af1462a8f3bb18d0768ee312c7dbf2a
King <king2934@gmail.com>
2019-01-20 00:34:45 +0800

    add Java

---
+++ Java/Studys/1.hello/hello.java
@@ -0,0 +1,8 @@
+public class hello
+{
+	//必须的
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+    }
+}
---
+++ Java/Studys/README.md
@@ -0,0 +1,2 @@
+# JAVA 编程语言
+	javac -encoding utf8 hello.java
