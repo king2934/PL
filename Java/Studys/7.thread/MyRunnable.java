c821969f7fbb48b54d66e9db01edeeb31216e6ae
king2934 <king2934@gmail.com>
2019-01-22 15:08:24 +0800

    update

--- Java/Studys/7.thread/MyRunnable.java
+++ Java/Studys/7.thread/MyRunnable.java
@@ -2,22 +2,38 @@ public class MyRunnable implements Runnable
 {
 	private Thread t;
 	private String threadName;
-   
+	
+	//
 	public static void main(String[] args)
 	{
-		MyRunnable mr = new MyRunnable();
-		mr.start();
+		MyRunnable mr1 = new MyRunnable(" Runnable - 1");
+		mr1.start();
+		System.out.println("R1 End ");
+		
+		MyRunnable mr2 = new MyRunnable(" Runnable - 2");
+		mr2.start();
+		System.out.println("R2 End ");
 	}
 	
+	//
 	public MyRunnable()
 	{
 		this.threadName = " Runnable - 0";
 	}
 	
-	public void run() 
+	//
+	public MyRunnable(String name)
 	{
+		this.threadName = name;
 	}
 	
+	//
+	public void run() 
+	{
+		System.out.println("Runing " +  threadName );
+	}
+	
+	//
 	public void start () 
 	{
       System.out.println("Starting " +  threadName );
