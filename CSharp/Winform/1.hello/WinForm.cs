0f178ceaf0e2bc66ed7545048143274e7a271704
king2934 <king2934@gmail.com>
2019-02-18 17:30:13 +0800

    add winform

---
+++ CSharp/Winform/1.hello/WinForm.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace hello
+{
+    public partial class WinForm : Form
+    {
+        public WinForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
