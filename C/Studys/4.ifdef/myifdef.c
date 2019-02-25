b93b75ac3d0be62cc74981170b26f6e57b3a23eb
king2934 <king2934@gmail.com>
2019-02-25 14:07:37 +0800

    update

--- C/Studys/4.ifdef/myifdef.c
+++ C/Studys/4.ifdef/myifdef.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 #ifdef _WIN64
-#define SNMAE "win64"
+#define SNMAE "Win64"
 #elif _WIN32
-#define SNMAE "win32"
+#define SNMAE "Win32"
 #elif __linux__
-#define SNMAE "linux"
+#define SNMAE "Linux"
 #endif
 int main()
 {
