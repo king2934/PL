ac392118572e2f731dc5abe11d36d133f55c13a5
king2934 <king2934@gmail.com>
2019-02-26 13:14:19 +0800

    update

--- C/Studys/5.librarys/librarys.c
+++ C/Studys/5.librarys/librarys.c
@@ -1,28 +1,32 @@
 #include <assert.h>
+#include <complex.h>
 #include <ctype.h>
 #include <errno.h>
+#include <fenv.h>
 #include <float.h>
+#include <inttypes.h>
+#include <iso646.h>
 #include <limits.h>
 #include <locale.h>
 #include <math.h>
 #include <setjmp.h>
 #include <signal.h>
+#include <stdalign.h>
 #include <stdarg.h>
+#include <stdatomic.h>
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdnoreturn.h>
 #include <string.h>
+#include <tgmath.h>
+#include <threads.h>
 #include <time.h>
-
-
-#include<sys/socket.h>
-#include<netinet/in.h>
-
-#include<unistd.h>
-
-#include<sys/types.h>
-#include<arpa/inet.h>
-
+#include <uchar.h>
+#include <wchar.h>
+#include <wctype.h>
 
 int main(int argc,char* argv[])
 {
