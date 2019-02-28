0aaf31dee60535f1a66f3eab0ebe67dc77d0f49d
king2934 <king2934@gmail.com>
2019-02-28 14:03:09 +0800

    update

--- C/Studys/0.net/1.cs/server.c
+++ C/Studys/0.net/1.cs/server.c
@@ -3,19 +3,31 @@
 #ifdef _WIN64
 /* windows 64 bit */
 #define SYSTEM_NAME "Win64"
+
+#include <winsock2.h>
+
 /* End windows 64 bit */
 #elif _WIN32
 /* windows 32 bit */
 #define SYSTEM_NAME "Win32"
+
+#include <winsock2.h>
+
 /* End windows 32 bit */
 #elif __linux__
 /* linux */
 #define SYSTEM_NAME "Linux"
+
+#include<sys/socket.h>
+
 /* End linux */
 #endif
 
 int main(int argc,char* argv[])
 {
 	printf("system %s \n",SYSTEM_NAME);
+	
+	int sock=socket(AF_INET,SOCK_STREAM,0);
+	
 	return 0;
 }
