7017aaa4f9bfb44473686ca015a6ba254c715306
king2934 <king2934@gmail.com>
2020-07-18 15:08:32 +0800

    打包jar包,包括images

--- Java/Studys/9.jframe.getResource/JFrameGetResource.java
+++ Java/Studys/9.jframe.getResource/JFrameGetResource.java
@@ -1,13 +1,38 @@
-public class JFrameGetResource
+import java.awt.*;
+import javax.swing.*;
+import java.net.URL;
+
+public class JFrameGetResource extends JFrame
 {
+	private URL imgURL;
+	private Image ImageTitle;
 	public static void main(String[] args)
 	{
-		JFrameGetResource JFGR = new JFrameGetResource();
-		System.out.println("JFrameGetResource:");
+		try {
+			new JFrameGetResource().setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public JFrameGetResource()
 	{
+		initialize();
+	}
+	
+	private void initialize()
+	{
+		//关闭窗体时 退出窗体
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		this.imgURL = JFrameGetResource.class.getResource("/images/image_title.png");
+		this.ImageTitle = Toolkit.getDefaultToolkit().getImage(imgURL);
+		this.setIconImage(this.ImageTitle);
+		
+		//setSize(int width, int height)
+		this.setSize(800,600);
+		this.setLayout(null);
+		this.setTitle("Java 窗体程序 标题");
+		this.setLocationRelativeTo(null);
 	}
 }
