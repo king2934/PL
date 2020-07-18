75e2570104b000145f06f2eea021191b0c962b0b
king2934 <king2934@gmail.com>
2020-07-18 14:37:08 +0800

    增加资源文件调用

---
+++ Java/Studys/9.jframe.getResource/JFrameGetResource.java
@@ -0,0 +1,13 @@
+public class JFrameGetResource
+{
+	public static void main(String[] args)
+	{
+		JFrameGetResource JFGR = new JFrameGetResource();
+		System.out.println("JFrameGetResource:");
+	}
+	
+	public JFrameGetResource()
+	{
+		
+	}
+}
