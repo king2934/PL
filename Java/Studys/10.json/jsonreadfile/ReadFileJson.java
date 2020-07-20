bbd096ac300958c892b8416f486040565f473991
king2934 <king2934@gmail.com>
2020-07-20 10:45:19 +0800

    read config.json

--- Java/Studys/10.json/jsonreadfile/ReadFileJson.java
+++ Java/Studys/10.json/jsonreadfile/ReadFileJson.java
@@ -1,4 +1,8 @@
+import java.io.*;
+
 import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 
 public class ReadFileJson
 {
@@ -10,5 +14,35 @@ public class ReadFileJson
 	
 	public ReadFileJson()
 	{
+		File file = new File("config.json");
+		if( file.exists() ){
+			System.out.println("config.json YES");
+			try
+			{
+				InputStream  is = new FileInputStream(file);
+				InputStreamReader isr = new InputStreamReader(is);
+				JsonReader jsonReader = new JsonReader(isr);
+				BufferedReader br = new BufferedReader(isr);
+				String result = "";
+				String line="";
+				while((line=br.readLine()) != null)
+				{
+					line = line.trim();
+					line = line.replaceAll("\n","");
+					line = line.replaceAll("\r","");
+					result += line;
+				}
+				isr.close();
+				System.out.println("result:"+result);
+			}
+			catch(IOException e)
+			{
+				
+			}
+		}else{
+			System.out.println("config.json is not fount!");
+		}
 	}
+	
+	
 }
