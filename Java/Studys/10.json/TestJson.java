0a1725184ec5ecf13c23b78e795cd90a85f3146f
king2934 <king2934@gmail.com>
2020-07-20 09:52:09 +0800

    json com.google.gson

---
+++ Java/Studys/10.json/TestJson.java
@@ -0,0 +1,13 @@
+public class TestJson
+{
+	public static void main(String[] args)
+	{
+		TestJson tj = new TestJson();
+		System.out.println("json com.google.gson");
+	}
+	
+	public TestJson()
+	{
+		
+	}
+}
