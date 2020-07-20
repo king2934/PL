1257462425605eb60d67d21335ead2f26b400c1b
king2934 <king2934@gmail.com>
2020-07-20 10:16:03 +0800

    增加读取config,json

---
+++ Java/Studys/10.json/jsonreadfile/ReadFileJson.java
@@ -0,0 +1,14 @@
+import com.google.gson.Gson;
+
+public class ReadFileJson
+{
+	public static void main(String[] args)
+	{
+		ReadFileJson rfj = new ReadFileJson();
+		System.out.println("read file for config.json");
+	}
+	
+	public ReadFileJson()
+	{
+	}
+}
